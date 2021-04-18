9287283a663ba856528ae90a0409e5f6b51fd0a0
Liam Liden <l14m@msn.com>
2021-04-18 12:34:33 -0500

    Test tiger

--- app/src/main/java/com/example/bottles/CameraActivity.java
+++ app/src/main/java/com/example/bottles/CameraActivity.java
@@ -16,78 +16,102 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
-public class CameraActivity extends AppCompatActivity {
-    private static final String TAG = CameraActivity.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
+import java.lang.ref.WeakReference;
+
+public class CameraActivity extends AppCompatActivity implements BaseArFragment.OnTapArPlaneListener {
 
     private ArFragment arFragment;
+    private Renderable model;
+    private ViewRenderable viewRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!checkIsSupportedDeviceOrFinish(this)) {
-            return;
+        setContentView(R.layout.camera_activity);
+        getSupportFragmentManager().addFragmentOnAttachListener((fragmentManager, fragment) -> {
+            if (fragment.getId() == R.id.arFragment) {
+                arFragment = (ArFragment) fragment;
+                arFragment.setOnTapArPlaneListener(CameraActivity.this);
+            }
+        });
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
         }
 
-        setContentView(R.layout.camera_activity);
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        loadModels();
+    }
 
-        arFragment.setOnTapArPlaneListener(
-                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    Anchor anchor = hitResult.createAnchor();
-                    placeObject(arFragment, anchor, Uri.parse("model.gltf"));
-
+    public void loadModels() {
+        WeakReference<CameraActivity> weakActivity = new WeakReference<>(this);
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("https://storage.googleapis.com/ar-answers-in-search-models/static/Tiger/model.glb"))
+                .setIsFilamentGltf(true)
+                .build()
+                .thenAccept(model -> {
+                    CameraActivity activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.model = model;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
+                });
+        ViewRenderable.builder()
+                .setView(this, R.layout.view_bottle)
+                .build()
+                .thenAccept(viewRenderable -> {
+                    CameraActivity activity = weakActivity.get();
+                    if (activity != null) {
+                        activity.viewRenderable = viewRenderable;
+                    }
+                })
+                .exceptionally(throwable -> {
+                    Toast.makeText(this, "Unable to load model", Toast.LENGTH_LONG).show();
+                    return null;
                 });
     }
 
-    private void placeObject(ArFragment arFragment, Anchor anchor, Uri uri) {
-        ModelRenderable.builder()
-                .setSource(arFragment.getContext(), uri)
-                .build()
-                .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
-                .exceptionally(throwable -> {
-                            Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
-                            return null;
-                        }
-
-                );
-
-    }
-
-    private void addNodeToScene(ArFragment arFragment, Anchor anchor, Renderable renderable) {
-        AnchorNode anchorNode = new AnchorNode(anchor);
-        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
-        node.setRenderable(renderable);
-        node.setParent(anchorNode);
-        arFragment.getArSceneView().getScene().addChild(anchorNode);
-        node.select();
-    }
-
-
-    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-//        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
-//            Log.e(TAG, "Sceneform requires Android N or later");
-//            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
-//            activity.finish();
-//            return false;
-//        }
-        String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
-            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
-                    .show();
-            activity.finish();
-            return false;
+    @Override
+    public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+        if (model == null || viewRenderable == null) {
+            Toast.makeText(this, "Loading...", Toast.LENGTH_SHORT).show();
+            return;
         }
-        return true;
+
+        // Create the Anchor.
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        // Create the transformable model and add it to the anchor.
+        TransformableNode model = new TransformableNode(arFragment.getTransformationSystem());
+        model.setParent(anchorNode);
+        model.setRenderable(this.model);
+        model.getRenderableInstance().animate(true).start();
+        model.select();
+
+        Node tigerTitleNode = new Node();
+        tigerTitleNode.setParent(model);
+        tigerTitleNode.setEnabled(false);
+        tigerTitleNode.setLocalPosition(new Vector3(0.0f, 1.0f, 0.0f));
+        tigerTitleNode.setRenderable(viewRenderable);
+        tigerTitleNode.setEnabled(true);
     }
 }

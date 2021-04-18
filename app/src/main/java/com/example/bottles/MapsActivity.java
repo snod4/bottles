4fb58774eb390fcd7e9b45343569f17dcd82cb22
Ryuta Kure <kureryut@grinnell.edu>
2021-04-17 23:27:06 -0500

    Add icon

--- app/src/main/java/com/example/bottles/MapsActivity.java
+++ app/src/main/java/com/example/bottles/MapsActivity.java
@@ -37,6 +37,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
@@ -146,8 +147,9 @@ public class MapsActivity extends AppCompatActivity
                                              //   bottleMap.put(b, b);
                                                Marker thisMarker = map.addMarker(new MarkerOptions()
                                                         .position(new LatLng(latitude, longitude))
-                                                        .draggable(false));
-                                               thisMarker.setTag(docId);
+                                                        .draggable(false)
+                                                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.bottleicon2)));
+                                                thisMarker.setTag(docId);
 
                                             }
                                         }
